b167e783daf3ac86395a23f2cec9594e599e0423
Bertho Erizal <berthoerizal21@gmail.com>
2019-06-16 23:39:16 +0700

    perbaikan deskripsi 2

--- application/views/layout/head.php
+++ application/views/layout/head.php
@@ -8,25 +8,6 @@ $site_config = $this->konfigurasi_model->listing(); ?>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <!-- Twitter -->
-  <meta name="twitter:site" content="@themepixels">
-  <meta name="twitter:creator" content="@themepixels">
-  <meta name="twitter:card" content="summary_large_image">
-  <meta name="twitter:title" content="Slim">
-  <meta name="twitter:description" content="Premium Quality and Responsive UI for Dashboard.">
-  <meta name="twitter:image" content="http://themepixels.me/slim/img/slim-social.png">
-
-  <!-- Facebook -->
-  <meta property="og:url" content="http://themepixels.me/slim">
-  <meta property="og:title" content="Slim">
-  <meta property="og:description" content="Premium Quality and Responsive UI for Dashboard.">
-
-  <meta property="og:image" content="http://themepixels.me/slim/img/slim-social.png">
-  <meta property="og:image:secure_url" content="http://themepixels.me/slim/img/slim-social.png">
-  <meta property="og:image:type" content="<?php echo base_url(); ?>assets/admin/image/png">
-  <meta property="og:image:width" content="1200">
-  <meta property="og:image:height" content="600">
-
   <!-- Meta -->
   <meta name="description" content="<?php echo $site_config->deskripsi ?>">
   <meta name="author" content="PDAK">
